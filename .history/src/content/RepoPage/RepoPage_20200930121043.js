ed10e1d5fa2bfa145fa6f94e285f9b9fb387a654
Prattyush Mangal <prattyushmangal@Prattyushs-MacBook-Pro.local>
2020-09-30 12:16:07 +0100

    React Step 2 table making

---
+++ .history/src/content/RepoPage/RepoPage_20200930121043.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  DataTable,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableExpandHeader,
+  TableHeader,
+  TableBody,
+  TableExpandRow,
+  TableCell,
+  TableExpandedRow,
+} from 'carbon-components-react';
+
+const RepoPage = () => {
+  return (
+    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+      <div className="bx--row repo-page__r1">
+        <div className="bx--col-lg-16">Data table will go here</div>
+      </div>
+    </div>
+  );
+};
+
+export default RepoPage;
